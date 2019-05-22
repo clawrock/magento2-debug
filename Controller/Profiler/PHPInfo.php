ab2340c35c962aec8d4be154b09476528fd557ad
makao <m.makalowski@gmail.com>
2019-05-22 22:32:01 +0200

    Fixed PHP info page #8

--- Controller/Profiler/PHPInfo.php
+++ Controller/Profiler/PHPInfo.php
@@ -3,11 +3,14 @@
 namespace ClawRock\Debug\Controller\Profiler;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
 
 class PHPInfo extends Action
 {
     public function execute()
     {
-        return phpinfo();
+        phpinfo();
+
+        return $this->resultFactory->create(ResultFactory::TYPE_RAW);
     }
 }
--- Test/Unit/Controller/Profiler/PHPInfoTest.php
+++ Test/Unit/Controller/Profiler/PHPInfoTest.php
@@ -3,6 +3,10 @@
 namespace ClawRock\Debug\Test\Unit\Controller\Profiler;
 
 use ClawRock\Debug\Controller\Profiler\PHPInfo;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use PHPUnit\Framework\TestCase;
 
@@ -10,10 +14,32 @@ class PHPInfoTest extends TestCase
 {
     public function testExecute()
     {
-        $controller = (new ObjectManager($this))->getObject(PHPInfo::class);
+        $contextMock = $this->getMockBuilder(Context::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $resultFactoryMock = $this->getMockBuilder(ResultFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $resultMock = $this->getMockBuilder(Raw::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $contextMock->expects($this->once())->method('getResultFactory')->willReturn($resultFactoryMock);
+
+        $resultFactoryMock->expects($this->once())->method('create')
+            ->with(ResultFactory::TYPE_RAW)
+            ->willReturn($resultMock);
+
+        $controller = (new ObjectManager($this))->getObject(PHPInfo::class, [
+            'context' => $contextMock,
+        ]);
+
         ob_start();
         $result = $controller->execute();
         ob_end_clean();
-        $this->assertTrue($result);
+
+        $this->assertInstanceOf(ResultInterface::class, $result);
     }
 }
